e08c89a5ab14002cefdc835179bda7702924a5ac
Arnav Mishra <arnmishra@gmail.com>
2016-10-07 14:02:32 -0500

    minor function changes

--- Framework/graph.py
+++ Framework/graph.py
@@ -8,22 +8,28 @@ http://www.bogotobogo.com/python/python_graph_data_structures.php
 class Node:
     """ Node class to store data in the graph.
 
-    Each Node stores an adjacency list
-    of all connected Nodes in the form of a dictionary that maps connected
-    Node to its weight. Each Node also stores the data pertaining to it.
+    Each Node stores an adjacency list of all connected Nodes in the form
+    of a dictionary that maps the connected node's key to its weight.
+    Each Node also stores the data pertaining to it.
     """
     def __init__(self, data):
         self.data = data
         self.connected_nodes = {}
 
-    def add_edge(self, node, weight):
-        self.connected_nodes[node] = weight
+    def add_edge(self, node_key, weight):
+        self.connected_nodes[node_key] = weight
+
+    def delete_edge(self, node_key):
+        del self.connected_nodes[node_key]
 
     def get_connected_nodes(self):
         return self.connected_nodes
 
     def get_data(self):
         return self.data
+
+    def set_data(self, data):
+        self.data = data
 
 
 class Graph:
@@ -37,18 +43,33 @@ class Graph:
     def __init__(self):
         self.nodes = {}
 
-    def add_node(self, key, data):
-        node = Node(data)
-        self.nodes[key] = node
-
     def get_all_nodes(self):
         return self.nodes
 
     def get_node(self, key):
-        return self.nodes[key].get_data()
+        if key in self.nodes:
+            return self.nodes[key]
+        else:
+            return None
+
+    def add_node(self, key, data):
+        node = Node(data)
+        self.nodes[key] = node
+
+    def set_node(self, key, data):
+        if key in self.nodes:
+            self.nodes[key].set_data(data)
+            return True
+        else:
+            return False
+
+    def delete_node(self, key):
+        del self.nodes[key]
 
     def add_connection(self, start_key, end_key, weight):
         start_node = self.nodes[start_key]
-        end_node = self.nodes[end_key]
-        start_node.add_edge(end_node, weight)
-        end_node.add_edge(start_node, weight)
+        start_node.add_edge(end_key, weight)
+
+    def delete_connection(self, start_key, end_key):
+        start_node = self.nodes[start_key]
+        start_node.delete_edge(end_key)
